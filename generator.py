331ad7c0c5bb5d8de0d78032d694798496c245a0
alex7280 <alexzhang802@gmail.com>
2023-04-27 20:37:27 -0400

    generate abalones

---
+++ generator.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import random
+
+
+
+df = pd.read_csv("data/abalone.data", delimiter=" ", header=None)
+
+df.iloc[0]
+
+min_max_collection = df.agg(['min', 'max'])
+
+def generator(df, max_size):
+    for i in range(0, max_size):
+        rand_num = random.random()
+        single_result = []
+        if(rand_num < 0.5):
+            gender = 'F'
+        else:
+            gender = 'M'
+        single_result.append(gender)
+        for j in range(1, len(df.iloc[0])):
+            single_result.append(random.uniform(min_max_collection[j][0] * 0.55, min_max_collection[j][1] * 1.18))
+        df.loc[len(df.index)] = single_result
+    return df
+        
+new_df = generator(df, 50000)
+new_df.to_csv("data/mass_abalone.data", sep=' ')
